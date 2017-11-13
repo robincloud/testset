95137ff2b02632222f589d0997cc7cf1ac4eb51e
MIKE <rlghd1996@gmail.com>
2017-11-13 20:32:39 +0900

    Fix bug

--- load_test.py
+++ load_test.py
@@ -8,14 +8,12 @@ import sys
 import tarfile
 
 POST_URL = 'https://robin-api.oneprice.co.kr/items'
-ITEM_COUNT = 0
-MALL_COUNT = 0
 
 
 def load(name):
     data = {}
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    tar = tarfile.open(dir_path + "/data/items.json.tar.gz")
+    tar = tarfile.open(dir_path + "\\data\\items.json.tar.gz")
     for member in tar.getmembers():
         if member.name == name + 'sec.json':
             f = tar.extractfile(member)
@@ -30,21 +28,18 @@ def load(name):
 
 
 def post(data):
+    mall_count = 0
     try:
-        global ITEM_COUNT
-        global MALL_COUNT
         requests.post(POST_URL, json=data)
-        ITEM_COUNT += 1
-        MALL_COUNT += data['nodes'].__len__()
+        mall_count = data['data'][0]['nodes'].__len__()
         print('Post success ', data['mid'])
     except Exception as e:
         print(e)
         print("failed to send: ", data['mid'])
+    return mall_count
 
 
 def run(name, thread):
-    global ITEM_COUNT
-    global MALL_COUNT
     item_list = load(name)
     print('--- Finish Loading ---')
     print('--- %d items loaded ---' % (item_list.__len__()))
@@ -52,14 +47,20 @@ def run(name, thread):
 
     print('--- Start Sending ---')
     start_time = time.time()
-    pool.map(post, item_list)
+    count_list = pool.map(post, item_list)
     print("--- %s seconds ---" % (time.time() - start_time))
-    print("--- Items: %d, Malls: %d ---" % (ITEM_COUNT, MALL_COUNT))
+    item_count = 0
+    mall_count = 0
+    for item in count_list:
+        item_count += 1
+        mall_count += item
+    print("--- Items: %d, Malls: %d ---" % (item_count, mall_count))
+    a=1
 
 
 if __name__ == '__main__':
     file_name = '10'
-    thread = 0
+    thread = 10
     try:
         opts, args = getopt.getopt(sys.argv[1:], "s:t:", ["sec=", "thread="])
     except getopt.GetoptError as err:
